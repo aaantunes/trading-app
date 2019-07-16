2bb76d718aa46d073a522333a13a2df4bb1829eb
aaantunes <aaantune@ryerson.ca>
2019-07-16 15:58:09 +0000

    Fixed maven dependency

--- src/main/java/ca/jrvs/apps/trading/model/domain/IexQuote.java
+++ src/main/java/ca/jrvs/apps/trading/model/domain/IexQuote.java
@@ -3,7 +3,7 @@ package ca.jrvs.apps.trading.model.domain;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
