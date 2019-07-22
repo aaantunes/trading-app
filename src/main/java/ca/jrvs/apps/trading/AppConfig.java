e0483ca54ded5b703dd176d13b802aa55bc9e0f4
aaantunes <aaantune@ryerson.ca>
2019-07-22 15:22:21 -0400

    Finished AppConfig

--- src/main/java/ca/jrvs/apps/trading/AppConfig.java
+++ src/main/java/ca/jrvs/apps/trading/AppConfig.java
@@ -1,11 +1,13 @@
 package ca.jrvs.apps.trading;
 
 import ca.jrvs.apps.trading.model.config.MarketDataConfig;
+import ca.jrvs.apps.trading.util.StringUtil;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,29 +18,47 @@ public class AppConfig {
 
     private Logger logger = LoggerFactory.getLogger(AppConfig.class);
 
-    // @Value("${iex.host}")
-    private String iex_host;
+//    @Value("${iex.host}")
+    private String iex_host = System.getenv("IEX_HOST");
 
-//    @Bean
-//    public PlatformTransactionManager txManager(DataSource dataSource) {}
 
     @Bean
     public MarketDataConfig marketDataConfig() {
+
+        if (StringUtil.isEmpty(System.getenv("IEX_PUB_TOKEN")) || StringUtil.isEmpty(iex_host)) {
+            throw new IllegalArgumentException("ENV:IEX_PUB_TOKEN or IEX_HOST is not set");
+        }
+
         MarketDataConfig marketDataConfig = new MarketDataConfig();
-        marketDataConfig.setHost("https://cloud.iexapis.com/stable");
-        marketDataConfig.setToken(System.getenv("IexToken"));
+        marketDataConfig.setHost(iex_host);
+        marketDataConfig.setToken(System.getenv("IEX_PUB_TOKEN"));
         return marketDataConfig;
     }
 
     @Bean
     public DataSource dataSource() {
-        return DataSourceBuilder
-                .create()
-                /*.username("")
-                .password("")
-                .url("")
-                .driverClassName("")*/
-                .build();
+
+        String jdbcUrl;
+        String username;
+        String password;
+
+        jdbcUrl = System.getenv("PSQL_URL"); //may have to remove jdbc: part
+        username = System.getenv("PSQL_USER");
+        password = System.getenv("PSQL_PASSWORD");
+
+        logger.error("JDBC:" + jdbcUrl);
+
+        if (StringUtil.isEmpty(jdbcUrl, username, password)) {
+            throw new IllegalArgumentException("Missing data source config env vars");
+        }
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
     }
 
     @Bean
